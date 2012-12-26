5f2f95fea0c301c1b9fcbb87af87fe023013de29
programmerby <aprisobal@gmail.com>
2012-12-26 22:53:07 +0300

    Golang's exp/html source code moved into tgz archive.

--- golang/prepare.sh
+++ golang/prepare.sh
@@ -3,11 +3,12 @@
 # Requirements: go >= 1.0
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-GOPATH=$DIR
+export GOPATH=$DIR
 
 # Download source code to $GOPATH/src/
 go get github.com/moovweb/gokogiri
 go get code.google.com/p/go-html-transform/h5
+tar -xzf go-2c8a88c1efce-exp-html.tar.gz -C $DIR/src
 
 # Compile & write binaries to $GOPATH/bin/
 go install bench_gokogiri
