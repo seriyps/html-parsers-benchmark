00d1aeaad69ff7721e6487784cb0b329cad07e28
Denis Tulskiy <tulskiy@gmail.com>
2012-12-27 20:30:50 +0600

    [java] read whole file before doing parsing

--- java/src/main/java/TestJSoup.java
+++ java/src/main/java/TestJSoup.java
@@ -3,6 +3,7 @@ import org.jsoup.nodes.Document;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * User: tulskiy
@@ -10,11 +11,16 @@ import java.io.IOException;
  */
 public class TestJSoup {
     public static void main(String[] args) throws IOException {
-        File file = new File(args[0]);
-        int iterations = Integer.parseInt(args[1]);
+        Scanner fi = new Scanner(new File(args[0]));
+        StringBuilder sb = new StringBuilder();
+        while (fi.hasNextLine()) {
+            sb.append(fi.nextLine()).append("\n");
+        }
+        String html = sb.toString();
         long time = System.currentTimeMillis();
+        int iterations = Integer.parseInt(args[1]);
         for (int i = 0; i < iterations; i++) {
-            Document document = Jsoup.parse(file, "utf-8");
+            Document document = Jsoup.parse(html);
         }
         System.out.println((System.currentTimeMillis() - time) / 1000f);
     }
