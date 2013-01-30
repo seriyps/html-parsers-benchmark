7c303e639952fde58c55fb33176727397e6644ae
pavel <pavel@192.168.1.103>
2013-01-31 01:00:23 +1300

    Added Python Scrapy parser. lib.sh fixed to work on MacOS.

--- lib.sh
+++ lib.sh
@@ -4,7 +4,12 @@ timeit () {
     # XXX: how to redirect time's output to stdout, but leave command's
     # errors on stderr? -o /dev/tty is ok in general, but causes problems
     # with GNU parallel
-    /usr/bin/time --format="real:%e	user:%U	sys:%S	max RSS:%M" $@ 2>&1
+    if [ ${OSTYPE//[0-9.]/} == 'darwin' ]; then
+    	# --format not supported in MacOS
+    	/usr/bin/time $@ 2>&1
+    else
+    	/usr/bin/time --format="real:%e	user:%U	sys:%S	max RSS:%M" $@ 2>&1
+    fi
 }
 
 print_header() {
--- python/prepare.sh
+++ python/prepare.sh
@@ -3,4 +3,4 @@
 virtualenv --system-site-packages env
 source env/bin/activate
 
-pip install lxml beautifulsoup4 BeautifulSoup html5lib
+pip install lxml beautifulsoup4 BeautifulSoup html5lib scrapy
